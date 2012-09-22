56485e78a20004df9cb629c7db7196df596ffdd9
Gary Larizza <gary@puppetlabs.com>
2012-09-22 10:52:33 -0700

    Use resource[:param] instead of @resource[:param]

--- lib/puppet/provider/property_list_key/rubycocoa.rb
+++ lib/puppet/provider/property_list_key/rubycocoa.rb
@@ -11,52 +11,49 @@ Puppet::Type.type(:property_list_key).provide(:rubycocoa) do
   defaultfor :operatingsystem => :darwin
 
   def exists?
-    return false unless File.file? @resource[:path]
-    if @resource[:path].nil? or @resource[:key].nil?
+    return false unless File.file? resource[:path]
+    if resource[:path].nil? or resource[:key].nil?
       fail("The 'key' and 'domain' parameters are required for the property_list_key type")
     end
 
-    plist = read_plist_file(@resource[:path])
-    plist.include? @resource[:key]
+    plist = read_plist_file(resource[:path])
+    plist.include? resource[:key]
   end
 
   def create
-    if File.file? @resource[:path]
-      plist = read_plist_file(@resource[:path])
+    if File.file? resource[:path]
+      plist = read_plist_file(resource[:path])
     else
       plist = OSX::NSMutableDictionary.alloc.init
     end
 
-    value_type = @resource[:value_type].downcase
-
-    case value_type
-    when 'integer'
-      plist_value = Integer(@resource[:value].first)
+    case resource[:value_type]
+    when :integer
+      plist_value = Integer(resource[:value].first)
     else
-      plist_value = @resource[:value].first
+      plist_value = resource[:value].first
     end
 
-    plist[@resource[:key]] = plist_value
+    plist[resource[:key]] = plist_value
 
-    write_plist_file(plist, @resource[:path])
+    write_plist_file(plist, resource[:path])
   end
 
   def destroy
-    if File.file?(@resource[:path])
-      plist = read_plist_file(@resource[:path])
+    if File.file?(resource[:path])
+      plist = read_plist_file(resource[:path])
     else
       return true
     end
 
-    plist.delete(@resource[:key])
+    plist.delete(resource[:key])
 
-    write_plist_file(plist, @resource[:path])
+    write_plist_file(plist, resource[:path])
   end
 
   def value
-    item_value = read_plist_file(@resource[:path])[@resource[:key]]
+    item_value = read_plist_file(resource[:path])[resource[:key]]
     klass =  item_value.to_ruby.class
-
     # The ugliness to make Puppet happy...
     case [klass]
     when [Fixnum]
@@ -69,20 +66,20 @@ Puppet::Type.type(:property_list_key).provide(:rubycocoa) do
   end
 
   def value=(item_value)
-    plist = read_plist_file(@resource[:path])
+    plist = read_plist_file(resource[:path])
 
     # Values out of Puppet are usually strings...except when they aren't.
     # They need to be massaged before writing to the plist
-    case @resource[:value_type].downcase
-    when 'integer'
-      plist[@resource[:key]] = Integer(item_value.first)
-    when 'array'
-      plist[@resource[:key]] = item_value
+    case resource[:value_type]
+    when :integer
+      plist[resource[:key]] = Integer(item_value.first)
+    when :array
+      plist[resource[:key]] = item_value
     else
-      plist[@resource[:key]] = item_value.first
+      plist[resource[:key]] = item_value.first
     end
 
-    write_plist_file(plist, @resource[:path])
+    write_plist_file(plist, resource[:path])
   end
 
   def read_plist_file(file_path)
